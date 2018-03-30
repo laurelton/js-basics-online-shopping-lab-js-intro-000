1409cad69b5159e4c23544a23fe05986d9ccd69c
Frank Rose <frankrosework@gmail.com>
2018-03-30 22:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100) + 1;
+ cart.push({
+   'itemName': item,
+   'itemPrice': price
+ });
+
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  var msg = 'In your cart, you have';
+  for (let i = 0; i < cart.length; i++) {
+    if (i === cart.length - 1 && cart.length > 1) {
+      msg += ` and ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    } else {
+      msg += ` ${cart[i].itemName} at $${cart[i].itemPrice},`;
+    }
+  }
+  msg += '.';
+
+  return cart.length === 0 ? 'Your shopping cart is empty.' : msg;
 }
 
 function total() {
