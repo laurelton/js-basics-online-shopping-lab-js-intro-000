4bfba397dbedb33214bdd4aff087f148c0efd9f9
Frank Rose <frankrosework@gmail.com>
2018-03-30 23:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`;
   }
 
-  var msg = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+  var msg = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   cart = [];
   return msg;
 }
